06e551efd64d67ee94e1afaf357032fa36b12a40
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2019-07-08 11:28:06 -0400

    minor change, having issue with Heroku/header auth

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const helmet = require('helmet')
 
 const POKEDEX = require('./pokedex.json')
 
-
-console.log(process.env.API_TOKEN);
-
 const app = express()
 
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
@@ -22,7 +19,6 @@ app.use(function validateBearerToken(req, res, next) {
 
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-    console.log('validate bearer token middleware');
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({error: 'Unauthorized request'})
